d9e86aa9e0b20cd92d6e09a6c0007a6d0471f807
AndronakyAndrew <andronakya@gmail.com>
2023-10-10 00:57:13 -0400

    Adding a Model Class and Connecting to the Database

---
+++ UsersApp/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersApp
+{
+    public class  ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+            optionsBuilder.UseNpgsql("Host=192.168.100.16;Port=5433;Database=UsersApp;Username=postgres;Password=1602");
+        }
+    }
+}
---
+++ UsersApp/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersApp
+{
+    public class User
+    {
+        public int userId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
