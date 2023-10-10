2d591f341488b223022393943fbec84699482537
AndronakyAndrew <andronakya@gmail.com>
2023-10-10 02:06:20 -0400

    Designing the registration window, retrieving data and displaying errors and creating migrations

--- UsersApp/MainWindow.xaml.cs
+++ UsersApp/MainWindow.xaml.cs
@@ -24,5 +24,47 @@ namespace UsersApp
         {
             InitializeComponent();
         }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string login = loginbox.Text.Trim();
+            string password = passwordbox.Password.Trim();
+            string password_2 = passwordbox_2.Password.Trim();
+            string email = emailbox.Text.Trim().ToLower();
+
+            if(login.Length < 6)
+            {
+                loginbox.ToolTip = "Login must be more than 6 characters";
+                loginbox.Background = Brushes.Red;
+            }
+            else if (password.Length < 7)
+            {
+                passwordbox.ToolTip = "Password must be more than 7 characters";
+                passwordbox.Background = Brushes.Red;
+            }
+            else if (password != password_2)
+            {
+                passwordbox_2.ToolTip = "Passwords don't match";
+                passwordbox_2.Background = Brushes.Red;
+            }
+            else if (!email.Contains("@") || !email.Contains("."))
+            {
+                emailbox.ToolTip = "Email isn't correct";
+                emailbox.Background = Brushes.Red;
+            }
+            else
+            {
+                loginbox.ToolTip = "";
+                loginbox.Background = Brushes.Transparent;
+                passwordbox.ToolTip = "";
+                passwordbox.Background = Brushes.Transparent;
+                passwordbox_2.ToolTip = "";
+                passwordbox_2.Background = Brushes.Transparent;
+                emailbox.ToolTip = "";
+                emailbox.Background = Brushes.Transparent;
+
+                MessageBox.Show("You have successfully registered");
+            }
+        }
     }
 }
